0a31510a817162e9188e55cbe59738154ff2954d
lilys0 <lsouryaphanh@gmail.com>
2021-09-19 20:25:47 +1000

    comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 // Current day display
 $("#currentDay").append(moment().format("dddd, MMMM Do"));
 
+// Save button event & save values into local storage
 var rowVal;
 var buttonTime;
 var saved;
@@ -8,8 +9,6 @@ $(".save-button").on("click", function (e) {
   e.preventDefault();
   buttonTime = $(this).data("time");
   rowVal = $(`#time${buttonTime}`).val();
-  console.log(rowVal, buttonTime);
-
   saved = localStorage.getItem("input");
   if (!saved) {
     saved = [];
@@ -17,24 +16,24 @@ $(".save-button").on("click", function (e) {
     saved = JSON.parse(saved);
   }
   saved[parseInt(buttonTime)] = rowVal;
-  console.log(saved);
   localStorage.setItem("input", JSON.stringify(saved));
 });
-
 var timeNow = parseInt(moment().format("H"));
 var rowTime;
 saved = localStorage.getItem("input");
 if (saved) {
   saved = JSON.parse(saved);
 }
+
+/* Timeblock is color coded to indicate whether it is in the past, present, or future.
+If user has saved added an note/appointment previously, it will be retrived from local storage */
 $(".block-time").each(function () {
   rowTime = parseInt($(this).data("time"));
   if (rowTime === timeNow) {
     $(`#time${rowTime}`).addClass("present");
-  }
-  if (rowTime < timeNow) {
+  } else if (rowTime < timeNow) {
     $(`#time${rowTime}`).addClass("past");
-  } else if (rowTime > timeNow) {
+  } else {
     $(`#time${rowTime}`).addClass("future");
   }
   if (saved) {
