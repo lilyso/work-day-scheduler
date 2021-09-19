7e5b641933d2825792a9bba12e9417f41d7cf64c
lilys0 <lsouryaphanh@gmail.com>
2021-09-19 17:00:56 +1000

    script colour change & css edit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,32 @@
 // Current day display
 $("#currentDay").append(moment().format("dddd, MMMM Do"));
 
+// console.log(test);
+
 // if textarea value does not equal null append content to textarea and save to local storage
 // if time does not equal current time or time in past dark colour & opacity textarea 0.5-75
 // if current time opacity 100% and bright colour
 //if time in future opacity 100% and dark colour
 
-var sixPm = $(".6pm").text();
-console.log(sixPm);
+var rowVal;
+var buttonTime;
+$(".save-button").on("click", function (e) {
+  e.preventDefault();
+  buttonTime = $(this).data("time");
+  rowVal = $(`#time${buttonTime}`).val();
+  localStorage.setItem();
+});
 
-var currentTime = moment().format("H");
-console.log(currentTime);
-
-function momentBlock() {
-  if (sixPm !== currentTime) {
-    console.log("no");
+var timeNow = parseInt(moment().format("H"));
+var rowTime;
+$(".block-time").each(function () {
+  rowTime = parseInt($(this).data("time"));
+  if (rowTime === timeNow) {
+    $(`#time${rowTime}`).addClass("present");
   }
-}
-
-momentBlock();
-
-// onclick
+  if (rowTime < timeNow) {
+    $(`#time${rowTime}`).addClass("past");
+  } else if (rowTime > timeNow) {
+    $(`#time${rowTime}`).addClass("future");
+  }
+});
